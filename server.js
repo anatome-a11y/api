d460c4b4d12946e0b413ba602fa5885fc54c8997
goveiat <goveiat@gmail.com>
2018-08-19 13:22:25 -0300

    inclisao de allow mwthods

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ const Anatomp = require('./anatomp');
 //API
 
 app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
