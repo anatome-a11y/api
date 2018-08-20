747b81b13ad8ea8a6f0cefe603d4e02ad2dca94c
goveiat <goveiat@gmail.com>
2018-08-20 08:52:29 -0300

    modelo anatomp

--- anatomp.js
+++ anatomp.js
@@ -1,14 +1,36 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var esquemaLocalizacao = new Schema({
+    anterior: String,
+    posterior: String,
+    latDir: String,
+    latEsq: String,
+    medDir: String,
+    medEsq: String,
+    superior: String,
+    inferior: String,
+});
+
 var esquemaMap = new Schema({
     id: String,
-    conteudo: String,
+    parte: String,
     numero: Number,
+    localizacao: esquemaLocalizacao
+});
+
+var esquemaPecaFisica = new Schema({
+    id: String,
+    nome: String,
+    descricao: String,
 });
 
 var esquemaAnatomp = new Schema({
     id: String,
+    nome: String,
+    instituicao: String,
+    roteiro: { type : Schema.Types.ObjectId, ref: 'Roteiro' },
+    pecasFisicas: [esquemaPecaFisica],
     mapa: [esquemaMap]
 });
 
