9129c7676bd6f2be7057cd91045568d20a9a2390
goveiat <goveiat@gmail.com>
2018-08-20 21:09:43 -0300

    populate

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.put('/peca/:_id', (req, res) => {
 
 //ROTEIRO
 app.get('/roteiro', (req, res) => {
-    Roteiro.find({}).populate('conteudos').exec((err, roteiros) => {
+    Roteiro.find({}).populate({path: 'conteudos', model: 'ConteudoTeorico'}).exec((err, roteiros) => {
         if (err) return res.status(500).send({status: 500, error: err});
 
         return res.status(200).send({status: 200, data: roteiros});
