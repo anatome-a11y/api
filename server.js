ce7ad9f3ab799dc82cd32b2939bf4386c8eafb03
goveiat <goveiat@gmail.com>
2018-12-10 10:01:52 -0200

    limite de resposta

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
 app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit: 50000}));
 
