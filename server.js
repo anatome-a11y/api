19f999367a7cdf4dab91784e195fd2d7de249670
goveiat <goveiat@gmail.com>
2018-12-10 09:53:18 -0200

    limite de resposta

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit: 50000}));
+
 //BANCO DE DADOS
 const mongoose = require('mongoose');
 
