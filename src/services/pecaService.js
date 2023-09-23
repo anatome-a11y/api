6e4ec6c6409f4c1c75ddfd6cdc1386b39ffa8389
Tassis Fernando <tsfernando72@gmail.com>
2023-09-23 11:21:16 -0300

    Corrige insercao de modelos

--- src/services/pecaService.js
+++ src/services/pecaService.js
@@ -5,106 +5,97 @@ const ConteudoTeorico = require('../models/conteudoTeorico');
 
 class PecaService {
     
-  async findAll(req, res) {
-    console.info("findAll pecas")
-    Peca.find({}).populate({ path: 'conteudoTeorico', populate: { path: 'partes'} }).populate({path: 'partes'}).exec((err, pecas) => {
-        if (err) {
-            console.error(err)
-            return res.status(500).send({status: 500, error: err});
-        }
-        console.info(pecas)
-        return res.status(200).send({status: 200, data: pecas});
-    });  
-  }
-
-  async create(req, res) {
-    var peca = req.body;
-    var partes = req.body.partes.map(c => new Parte(c));
-    var conteudo = req.body.conteudoTeorico.map(c => new ConteudoTeorico(c));
-
-    console.info("Requisição recebida: ", peca)
-
-    //Salva as partes das peças
-    Parte.collection.insertMany(partes, (err, partes) => {
-        if (err) {
-            console.log(err)
-            return res.status(500).send({status: 500, error: err});
-        }
-
-        peca.partes = peca.partes.map(c => c._id)
-
-        //Salva os conteúdos teóricos
-        ConteudoTeorico.collection.insertOne(conteudo, (err, conteudos) => {
+    async findAll(req, res) {
+        console.info("findAll pecas")
+        Peca.find({}).populate({ path: 'conteudoTeorico', populate: { path: 'partes'} }).populate({path: 'partes'}).exec((err, pecas) => {
             if (err) return res.status(500).send({status: 500, error: err});
-    
-            peca.conteudoTeorico = peca.conteudoTeorico.map(c => c._id)
-    
-            const toSave = new Peca(peca)        
-    
-            toSave.save((err, _peca) => {
+
+            return res.status(200).send({status: 200, data: pecas});
+        });
+    }
+
+    async create(req, res) {
+        var peca = req.body;
+        var partes = req.body.partes.map(c => new Parte(c));
+        var conteudo = req.body.conteudoTeorico.map(c => new ConteudoTeorico(c));
+
+        //Salva as partes das peças
+        Parte.collection.insert(partes, (err, partes) => {
+            if (err) return res.status(500).send({status: 500, error: err});
+
+            peca.partes = peca.partes.map(c => c._id)
+
+            //Salva os conteúdos teóricos
+            ConteudoTeorico.collection.insert(conteudo, (err, conteudos) => {
                 if (err) return res.status(500).send({status: 500, error: err});
         
-                return res.status(200).send({status: 200, data: _peca});
-            }); 
-        });
-    });
-  }
-
-  async delete(req, res) {
-    Peca.findById(req.params._id, (err, peca) => {
-        Roteiro.find({}, (err, roteiros) => {
-            const achou = roteiros.filter(r => r.partes.some(id => peca.partes.indexOf(id) >= 0));
-
-            if (achou.length > 0) {
-                const nomes = achou.map(r => r.nome).join(', ');
-                return res.status(500).send({status: 500, error: 'Não é possível excluir a peça, pois a mesma possui partes utilizadas nos roteiros '+nomes+'.'}); 
-            } else{
-                Peca.findByIdAndRemove(req.params._id, (err, _peca) => {
+                peca.conteudoTeorico = peca.conteudoTeorico.map(c => c._id)
+        
+                const toSave = new Peca(peca)        
+        
+                toSave.save((err, _peca) => {
                     if (err) return res.status(500).send({status: 500, error: err});
+            
+                    return res.status(200).send({status: 200, data: _peca});
+                }); 
+            });
+        });
+    }
 
-                    Parte.remove({_id: {$in: _peca.partes}}, err => {
+    async delete(req, res) {
+        Peca.findById(req.params._id, (err, peca) => {
+            Roteiro.find({}, (err, roteiros) => {
+                const achou = roteiros.filter(r => r.partes.some(id => peca.partes.indexOf(id) >= 0));
+
+                if(achou.length > 0){
+                    const nomes = achou.map(r => r.nome).join(', ');
+                    return res.status(500).send({status: 500, error: 'Não é possível excluir a peça, pois a mesma possui partes utilizadas nos roteiros '+nomes+'.'}); 
+                }else{
+                    Peca.findByIdAndRemove(req.params._id, (err, _peca) => {
                         if (err) return res.status(500).send({status: 500, error: err});
 
-                        ConteudoTeorico.remove({_id: {$in: _peca.conteudoTeorico}}, err => {
+                        Parte.remove({_id: {$in: _peca.partes}}, err => {
                             if (err) return res.status(500).send({status: 500, error: err});
 
-                            return res.status(200).send({status: 200, data: _peca});
-                        })                        
-                    })   
-                })
-            }           
-        })         
-    })
-  }
+                            ConteudoTeorico.remove({_id: {$in: _peca.conteudoTeorico}}, err => {
+                                if (err) return res.status(500).send({status: 500, error: err});
 
-  async update(req, res) {
-    var peca = req.body;
-    var partes = req.body.partes;
-    var conteudo = req.body.conteudoTeorico;
-
-
-    partes.forEach(p => {
-        Parte.findByIdAndUpdate(p._id, p, {upsert: true}, (err, _parte) => {
-            if (err) return res.status(500).send({status: 500, error: err});
+                                return res.status(200).send({status: 200, data: _peca});
+                            })                        
+                        })   
+                    })
+                }           
+            })         
         })
-    })
+    }
 
-    conteudo.forEach(p => {
-        ConteudoTeorico.findByIdAndUpdate(p._id, p, {upsert: true}, (err, _conteudo) => {
-            if (err) return res.status(500).send({status: 500, error: err});
+    async update(req, res) {
+        var peca = req.body;
+        var partes = req.body.partes;
+        var conteudo = req.body.conteudoTeorico;
+
+
+        partes.forEach(p => {
+            Parte.findByIdAndUpdate(p._id, p, {upsert: true}, (err, _parte) => {
+                if (err) return res.status(500).send({status: 500, error: err});
+            })
         })
-    })   
-    
-    peca.partes = partes.map(c => c._id)
-    peca.conteudoTeorico = conteudo.map(c => c._id)    
 
-    Peca.findByIdAndUpdate(peca._id, peca, (err, _peca) => {
-        if (err) return res.status(500).send({status: 500, error: err});
+        conteudo.forEach(p => {
+            ConteudoTeorico.findByIdAndUpdate(p._id, p, {upsert: true}, (err, _conteudo) => {
+                if (err) return res.status(500).send({status: 500, error: err});
+            })
+        })   
+        
+        peca.partes = partes.map(c => c._id)
+        peca.conteudoTeorico = conteudo.map(c => c._id)    
 
-        return res.status(200).send({status: 200, data: _peca});
-    })
-  }
+        Peca.findByIdAndUpdate(peca._id, peca, (err, _peca) => {
+            if (err) return res.status(500).send({status: 500, error: err});
 
+            return res.status(200).send({status: 200, data: _peca});
+        })
+    }
 }
 
 module.exports = PecaService
--- src/services/roteiroService.js
+++ src/services/roteiroService.js
@@ -5,71 +5,71 @@ const withResumoMidias = require('../utils/midiaUtils')
 
 class RoteiroService {
 
-  async findall(req, res) {
-    Roteiro.find({})
-      .populate({ path: 'conteudos', populate: {path: 'partes'} })
-      .populate({path: 'partes'}).lean().exec((err, roteiros) => {
-        if (err) return res.status(500).send({status: 500, error: err});
-
-        const _roteiros = roteiros.map(withResumoMidias);
-
-        Peca.find({}).populate({path: 'partes'}).lean().exec((err, pecas) => {
+    async findall(req, res) {
+        Roteiro.find({})
+        .populate({ path: 'conteudos', populate: {path: 'partes'} })
+        .populate({path: 'partes'}).lean().exec((err, roteiros) => {
             if (err) return res.status(500).send({status: 500, error: err});
 
-            //Provisório: No futuro, salvar referencia de peça generica dentro de parte
-            const data = _roteiros.map(roteiro => {
-                let pecasAnatomp = {};
+            const _roteiros = roteiros.map(withResumoMidias);
 
-                pecas.forEach(peca => {
-                    roteiro.partes.forEach(parteAnatomp => {
-                        peca.partes.forEach(partePeca => {
-                            if(partePeca._id == parteAnatomp._id && !pecasAnatomp.hasOwnProperty(peca._id)){
-                                pecasAnatomp[peca._id] = peca;
-                            }
-                        })                        
-                    })
-                })
-
-                return {...roteiro, pecasGenericas: Object.values(pecasAnatomp)}
-            })       
-    
-            return res.status(200).send({status: 200, data});
-        });        
-    });  
-  }
-
-  async create(req, res) {
-    const roteiro = new Roteiro(req.body)
-    roteiro.save((err, _roteiro) => {
-        if (err) return res.status(500).send({status: 500, error: err});
-
-        return res.status(200).send({status: 200, data: _roteiro});
-    }); 
-  }
-
-  async delete(req, res) {
-    Anatomp.find({roteiro: req.params._id}, (err, anatomps) => {
-        if(anatomps.length > 0){
-            const nomes = anatomps.map(a => a.nome).join(', ');
-            return res.status(401).send({status: 401, error: 'Não é possível excluir o roteiro, pois o mesmo está vinculado às Anatoms '+nomes+'.'}); 
-        }else{
-            Roteiro.findByIdAndRemove(req.params._id, (err, _roteiro) => {
+            Peca.find({}).populate({path: 'partes'}).lean().exec((err, pecas) => {
                 if (err) return res.status(500).send({status: 500, error: err});
+
+                //Provisório: No futuro, salvar referencia de peça generica dentro de parte
+                const data = _roteiros.map(roteiro => {
+                    let pecasAnatomp = {};
+
+                    pecas.forEach(peca => {
+                        roteiro.partes.forEach(parteAnatomp => {
+                            peca.partes.forEach(partePeca => {
+                                if(partePeca._id == parteAnatomp._id && !pecasAnatomp.hasOwnProperty(peca._id)){
+                                    pecasAnatomp[peca._id] = peca;
+                                }
+                            })                        
+                        })
+                    })
+
+                    return {...roteiro, pecasGenericas: Object.values(pecasAnatomp)}
+                })       
         
-                return res.status(200).send({status: 200, data: _roteiro});
-            })
-        }
-    })
-  }
+                return res.status(200).send({status: 200, data});
+            });        
+        });  
+    }
 
-  async update(req, res) {
-    const roteiro = new Roteiro(req.body)
-    Roteiro.findByIdAndUpdate(req.params._id, roteiro, (err, _roteiro) => {
-        if (err) return res.status(500).send({status: 500, error: err});
+    async create(req, res) {
+        const roteiro = new Roteiro(req.body)
+        roteiro.save((err, _roteiro) => {
+            if (err) return res.status(500).send({status: 500, error: err});
 
-        return res.status(200).send({status: 200, data: _roteiro});
-    })
-  }
+            return res.status(200).send({status: 200, data: _roteiro});
+        });  
+    }
+
+    async delete(req, res) {
+        Anatomp.find({roteiro: req.params._id}, (err, anatomps) => {
+            if(anatomps.length > 0){
+                const nomes = anatomps.map(a => a.nome).join(', ');
+                return res.status(401).send({status: 401, error: 'Não é possível excluir o roteiro, pois o mesmo está vinculado às Anatoms '+nomes+'.'}); 
+            }else{
+                Roteiro.findByIdAndRemove(req.params._id, (err, _roteiro) => {
+                    if (err) return res.status(500).send({status: 500, error: err});
+            
+                    return res.status(200).send({status: 200, data: _roteiro});
+                })
+            }
+        })
+    }
+
+    async update(req, res) {
+        const roteiro = new Roteiro(req.body)
+        Roteiro.findByIdAndUpdate(req.params._id, roteiro, (err, _roteiro) => {
+            if (err) return res.status(500).send({status: 500, error: err});
+
+            return res.status(200).send({status: 200, data: _roteiro});
+        })
+    }
 }
 
 module.exports = RoteiroService;
